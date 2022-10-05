49f9a56d4b7448fbd77644d64e44f497ddbafc5f
Ishan <ishan779mishra@gmail.com>
2022-10-05 21:56:06 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById("greeting").innerHTML= new Date().getHours() >=12  ? 'Good Afternoon' : 'Good Morning'
+document.getElementById("greeting").innerHTML= new Date().getHours() >=18  ? 'Good Evening' : 'Good Afternoon'
